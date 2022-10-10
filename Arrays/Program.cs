d812e3fddc3b20de8770c7305410641602b34c3e
Giorgi Kveladze <g.qvelladze@gmail.com>
2022-10-10 11:41:13 +0400

    Add project files.

---
+++ Arrays/Program.cs
@@ -0,0 +1,89 @@
+﻿// Array
+
+// int[] myArray = new int[6];
+
+// int[] myArray = { 1, 2, 3, 4, 5, 6 };
+
+//string[] myWeekDays = {"Monday", "Tueday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+//myWeekDays[1] = "Tuesday";
+
+
+//int[] myArray = new int[6];
+//myArray[0] = 13;
+//myArray[3] = 14;
+//Console.WriteLine(myArray);
+
+
+
+
+//int[] myArray = new int[5];
+//for (int i = 0; i < myArray.Length; i++) {
+//    myArray[i] = i;
+//}
+
+
+
+//int[] array = { 1, 2, 3, 4, 5 };
+//int length = array.Length;
+
+//int[] reversed = new int[length];
+
+//for (int index = 0; index < length; index++) {
+//    reversed[length - index - 1] = array[index];
+//}
+
+//for (int index =0; index<length; index++)
+//{
+
+//    Console.Write(reversed[index] + " ");
+//}
+
+
+
+
+//int n = int.Parse(Console.ReadLine());
+//int[] array = new int[n];
+
+//for (int i = 0; i < n; i++) {
+//    array[i] = int.Parse(Console.ReadLine());
+//}
+
+
+
+
+//Console.Write("Enter a positive integer: ");
+//int n = int.Parse(Console.ReadLine());
+//int[] array = new int[n];
+
+//for (int i = 0; i < n; i++)
+//{
+//    array[i] = int.Parse(Console.ReadLine());
+//}
+
+//bool symetric = true;
+//for (int i = 0; i < array.Length / 2; i++)
+//{
+
+//    if (array[i] != array[n - i - 1])
+//    {
+//        symetric = false;
+//        continue;
+//    }
+//}
+//Console.WriteLine("Is symetric? {0}", symetric);
+
+
+
+
+
+//string[] array = { "one", "two", "three", "four" };
+
+//for (int index = 0; index < array.Length; index++)
+//{
+//    Console.WriteLine("Element[{0}] = {1}", index, array[index]);
+//}
+
+
+
+
